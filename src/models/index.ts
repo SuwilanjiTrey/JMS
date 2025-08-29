a4e27b908e5b3c0af4e1f5e6d10ffc54533e4cae
Francis <kalungafrancis28@gmail.com>
2025-08-29 18:46:39 +0200

    Remaned document model file; updated index exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,9 @@
+// src/models/index.ts
+
+// Export everything from existing models
 export * from './Role';
 export * from './User';
 export * from './Case';
+
+// Export Document model
+export * from './Document';
